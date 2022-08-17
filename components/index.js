415cbcacfe9c96ed2489bf05d944cee48ef75c73
adnanerlansyah403 <adnanerlansyah403@gmail.com>
2022-08-17 11:36:16 +0700

    Project Ecommerce - NextJS

---
+++ components/index.js
@@ -0,0 +1,18 @@
+import Cart from './Cart';
+import Product from './Product';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import HeroBanner from './HeroBanner';
+import FooterBanner from './FooterBanner';
+import Layout  from './Layout';
+
+
+export {
+    Cart,
+    Navbar,
+    Footer,
+    FooterBanner,
+    HeroBanner,
+    Product,
+    Layout,
+}
